71ae818688cff1b33856d075a933512f46bf7dce
Casneil Simpson <casneil.simpson@gmail.com>
2019-12-30 11:21:51 +0100

    adding logic to Deck component

--- components/DeckView.js
+++ components/DeckView.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { getData } from "../utils/api";
 
 const DeckView = props => {
+  const deck = props.navigation.state.params.entryId;
+  console.log(deck);
+  const decks = getData();
+
   return (
     <View style={styles.container}>
-      <Text>Hello from Deck View</Text>
+      <Text>{decks[deck].title}</Text>
+      <Text>{decks[deck].questions.length}</Text>
     </View>
   );
 };
